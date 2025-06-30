70ed4bb0cdec12f157073b470809941c5d36d5a5
JohnOgama <jogama696@gmail.com>
2025-06-30 16:25:15 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <h1 className="text-red-500">Initial commit</h1>
+    </div>
+  );
+}
+
+export default App;
